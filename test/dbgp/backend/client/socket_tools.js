c70c5bc5efb85d40afe9b819ddc1207316c2a3d9
Danny Berger <dpb587@gmail.com>
2013-05-08 11:30:20 -0600

    add moca test framework; extract dbgp client socket commands for better testability

---
+++ test/dbgp/backend/client/socket_tools.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+
+var DbgpBackendClientSocketTools = require('../../../../lib/dbgp/backend/client/socket_tools');
+
+function createSocketSendMessageTest(args, expected) {
+    return function (done) {
+        var socket = {};
+
+        socket.tidebugCallbacks = {};
+        socket.tidebugSendCount = 2;
+        socket.write = function (actual) {
+            assert.equal(actual, expected);
+            done();
+        };
+
+        DbgpBackendClientSocketTools.socketSendMessage.apply(socket, args);
+    }
+};
+
+describe('dbgp/backend/client/socket_tool', function () {
+    describe('#socketSendMessage', function () {
+        it(
+            'includes transaction IDs',
+            createSocketSendMessageTest(
+                [ 'step_over' ],
+                'step_over -i 3\0'
+            )
+        );
+
+        it(
+            'includes arbitrary arguments',
+            createSocketSendMessageTest(
+                [ 'context_names', { d : 0 } ],
+                'context_names -i 3 -d 0\0'
+            )
+        );
+
+        it(
+            'encodes data when present',
+            createSocketSendMessageTest(
+                [ 'property_set', { d : 0, n : '$testme', t : 'string' }, 'special value' ],
+                'property_set -i 3 -d 0 -n "$testme" -t "string" -- c3BlY2lhbCB2YWx1ZQ==\0'
+            )
+        );
+    });
+});
